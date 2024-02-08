97b1b06766800d1351577c467a94e6c99e1e327a
kelvinko <kelvinkoko@gmail.com>
2024-02-08 21:55:59 +0900

    Add rss fetcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
-// Webpack 5 not work
-// workaround ref: https://github.com/rbren/rss-parser/issues/249
-// import Parser from "../node_modules/rss-parser/dist/rss-parser.min.js";
 import styles from "./App.module.css";
+import { NewsItem } from "./DataModel/NewsItem";
 import bgImage from "./Image/default-bg.jpg";
+import { fetchRss } from "./Rss/RssFetcher";
 import MainNews from "./Ui/MainNews";
 import NewsInfo from "./Ui/NewsInfo";
 import NewsPreview from "./Ui/NewsPreview";
@@ -19,6 +18,14 @@ const App = () => {
       "https://techcrunch.com/wp-content/uploads/2015/02/cropped-cropped-favicon-gradient.png?w=32",
     time: "2 hours ago"
   };
+  const [items, setItems] = React.useState<NewsItem[]>([item]);
+  let [mainItem, ...previewItems] = items;
+
+  React.useEffect(() => {
+    (async () => {
+      setItems(await fetchRss("https://techcrunch.com/feed/", 5));
+    })();
+  }, []);
   return (
     <>
       <img className={styles.bg} src={bgImage} />
@@ -26,16 +33,14 @@ const App = () => {
         <div className={styles.title}>Tech News</div>
         <div className={styles.windowContent}>
           <div className={styles.mainNewsContent}>
-            <MainNews item={item} />
-            <NewsInfo item={item} />
+            <MainNews item={mainItem} />
+            <NewsInfo item={mainItem} />
           </div>
           <div className={styles.divider} />
           <div className={styles.previewList}>
-            <NewsPreview item={item} />
-            <NewsPreview item={item} />
-            <NewsPreview item={item} />
-            <NewsPreview item={item} />
-            <NewsPreview item={item} />
+            {previewItems.map((item, i) => (
+              <NewsPreview item={item} key={i} />
+            ))}
           </div>
         </div>
       </div>
---
+++ src/Rss/RssFetcher.ts
@@ -0,0 +1,69 @@
+import * as lodash from "lodash";
+import { NewsItem } from "../DataModel/NewsItem";
+// Webpack 5 not work
+// workaround ref: https://github.com/rbren/rss-parser/issues/249
+import * as moment from "moment";
+import Parser from "../../node_modules/rss-parser/dist/rss-parser.min.js";
+
+const CORS_PROXY = "https://corsproxy.io/?";
+
+export async function fetchRss(
+  feedUrl: string,
+  amount: number
+): Promise<NewsItem[]> {
+  const parser = new Parser();
+  const feed = await parser.parseURL(`${CORS_PROXY}${feedUrl}`);
+
+  const filtered = lodash
+    .chain(feed.items)
+    .orderBy("isoDate", "desc")
+    .take(amount)
+    .value();
+
+  return await Promise.all<NewsItem>(
+    filtered.map(async (item): Promise<NewsItem> => {
+      const imageUrl = await extractRepresentativeImage(
+        `${CORS_PROXY}${item.link}`
+      );
+      return {
+        title: item.title,
+        thumbnailUrl: imageUrl,
+        time: moment(item.pubDate, "ddd, DD MMM YYYY HH:mm:ss ZZ").fromNow(),
+        source: feed.title,
+        sourceLogoUrl: feed.image.url
+      };
+    })
+  );
+}
+
+async function extractRepresentativeImage(
+  url: string
+): Promise<string | undefined> {
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = html;
+
+    const imageElement =
+      tempElement.querySelector('meta[property="og:image"]') ||
+      tempElement.querySelector('meta[name="twitter:image"]') ||
+      tempElement.querySelector("img");
+
+    if (imageElement) {
+      const imageUrl =
+        imageElement.getAttribute("src") ||
+        imageElement.getAttribute("content");
+
+      if (imageUrl) {
+        return imageUrl;
+      }
+    }
+
+    return undefined;
+  } catch (error) {
+    console.error("Error extracting representative image:", error);
+    return undefined;
+  }
+}
